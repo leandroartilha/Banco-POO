4b92c11ae53ebc38c144c71066a5cedc0f3676f8
Leandro Artilha <87547427+leandroartilha@users.noreply.github.com>
2022-07-16 02:00:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,12 +6,6 @@
         static void Main(string[] args)
         {
 
-            Controlador p = new ControleRemoto();
-
-            p.ligar();
-            p.maisVolume();
-            p.ligarMudo();
-            p.abrirMenu();
 
         }
     }
